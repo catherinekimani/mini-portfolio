9f96c6b7a4616ffb43bfc793fb8ece952e17fd3b
catherinekimani <catherinekimani882@gmail.com>
2022-08-16 02:14:11 +0300

    Add Home url

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,9 @@ const Navbar = () => {
         <div class="overlay"></div>
         <div class="container-fluid">
             <nav class="">
-                <div class='brand'><a href="/"><img src="https://cdn-icons-png.flaticon.com/128/5243/5243360.png" alt="" width={'45px'}></img></a></div>
+                <div class='brand'><a href="/."><img src="https://cdn-icons-png.flaticon.com/128/5243/5243360.png" alt="" width={'45px'}></img></a></div>
                 <ul>
-                    <li><a href="/"><span>01.</span>Home</a></li>
+                    <li><a href="/."><span>01.</span>Home</a></li>
                     <li><a href="#About"><span>02.</span>About</a></li>
                     <li><a href="#portfolio"><span>03.</span>Portfolio</a></li>
                     <li><a href="#contact"><span>04.</span>Contact</a></li>
