f30d5f836c09a0135545e8772764a70c4f722215
catherinekimani <catherinekimani882@gmail.com>
2022-08-16 02:01:50 +0300

    Update contact and Home component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,26 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='contact'>
+      <div className='row'>
+      <div class="col-md-6 initial-1 aos-init" data-aos="zoom-in" data-aos-delay="300">
       <div id='contact'>
           <h2><span>04.</span>Let's talk</h2>
           <h2 className='title'>Get in touch</h2>
           <p className='p'>Wanna talk about a project or just saying hi</p>
-          <i class="fa fa-comments"><span id="io">Say Hello</span></i> 
-          <a  className='reach' href='Email: catherinekimani882@gmail.com'>  catherinekimani882@gmail</a>
+          <a  className='reach' href='Email: catherinekimani882@gmail.com'><span id="io">Say Hello</span>: catherinekimani882@gmail</a>
+          </div>
+          </div>
+      <div class="col-md-6 initial-1 aos-init" data-aos="zoom-in" data-aos-delay="300">
+        <img src="./cont.png" alt="" class="img-fluid" width="50%" height="50%"></img>
+        </div>
       </div>
+      <p className='footer'>Made by Catherine Kimani <br />💕</p>
+      <div class="up-arrow ">
+            <div class="arrow ">
+                <a href="/"><i class="fa fa-angle-up "></i>
+            </a>
+            </div>
+        </div>
       </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,8 @@ const Home = () => {
         <div className='container text-light'>
           <h5 class="mt-5">Hello, my name is</h5>
           <h1>Catherine Kimani. </h1>
-          <h4>I am a Software Developer</h4>
-          <p>I’m a software engineer with experience in both Front-end and and Back-end Development, I have experience in  various programmig languages icluding;Python,Typescript,JavaScript etc. Currently, I’m focused on the Back-end Development using Python and Django.</p>
+          <h4>Junior Software Developer</h4>
+          <p>I’m a software engineer with experience in both Front-end and and Back-end Development, I have experience in  various programmig languages including;Python,Typescript,JavaScript etc. Currently, I’m focused on the Back-end Development using Python and Django.</p>
           
         </div>
         <a href="about" className='email'>Contact me</a>
