7f16f564232f7c83e44e83fbf3e0e75b553aa536
catherinekimani <catherinekimani882@gmail.com>
2022-08-17 02:21:18 +0300

    Fix bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div id='Home'>
+    
     <div className='row container mx-auto'>
       <div className='col-md-12'>
         <div className='container text-light'>
@@ -12,10 +12,10 @@ const Home = () => {
           <p>I’m a software engineer with experience in both Front-end and and Back-end Development, I have experience in  various programmig languages including;Python,Typescript,JavaScript etc. Currently, I’m focused on the Back-end Development using Python and Django.</p>
           
         </div>
-        <a href="about" className='email'>Contact me</a>
+        <a href="mailto:catherinekimani882@gmail.com" className='email'>Contact me</a>
       </div>
       </div>
-      </div>
+      
   )
 }
 
