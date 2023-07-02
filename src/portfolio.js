dcaafdcf47dd55c8dccd13827856f0213f80facc
catherinekimani <catherinekimani882@gmail.com>
2023-07-02 15:55:03 +0300

    Ft: Set up portfolio.js file with header,about,projects,skills & contact sections

---
+++ src/portfolio.js
@@ -0,0 +1,64 @@
+const header = {
+	homepage: 'https://catherinekimani.github.io/mini-project/',
+	title: 'CK.',
+}
+
+const about = {
+name: 'Catherine Kimani',
+role: 'Software Developer',
+description:
+    "'I'm a talented and impact-driven Software Developer specializing in creating tailored solutions that help individuals and businesses achieve their strategic goals. With a diverse skillset in programming languages, agile methodologies, version control, and database management, I deliver exceptional results.",
+resume: 'https://example.com',
+social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com',
+},
+}
+
+const projects = [
+{
+    name: 'Project 1',
+    description:
+    '🔜 Unveiling Exciting Projects Soon! Stay Tuned',
+    stack: [],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+},
+{
+    name: 'Project 2',
+    description:
+    '🔜 Unveiling Exciting Projects Soon! Stay Tuned',
+    stack: [],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+},
+{
+    name: 'Project 3',
+    description:
+    '🔜 Unveiling Exciting Projects Soon! Stay Tuned',
+    stack: [],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+},
+]
+
+const skills = [
+	'HTML',
+	'CSS',
+	'JavaScript',
+	'Python',
+    'Django',
+    'Flask',
+	'Angular',
+	'TypeScript',
+	'React',
+    'PostgreSAL',
+    'C',
+	'Git',
+]
+
+const contact = {
+	email: 'catherinekimani882@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
