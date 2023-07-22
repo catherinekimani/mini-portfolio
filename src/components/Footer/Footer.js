79301a62ff44483e4136d55e7c6003efeef61e15
catherinekimani <catherinekimani882@gmail.com>
2023-07-22 05:30:00 -0600

    Create footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://catherinekimani.github.io/mini-project/'
+      className='link footer__link'
+    >
+      Unleashing Creativity ❤️ Inspiring Possibilities
+    </a>
+  </footer>
+)
+
+export default Footer
